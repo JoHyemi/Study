afd5e67257bc68fd3e1fcd63a2f08f063adb9729
JoHyemi <92564768+JoHyemi@users.noreply.github.com>
2021-12-03 08:05:49 +0900

    concat

---
+++ JS100/009.js
@@ -0,0 +1,21 @@
+//다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+
+//데이터
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/', month,'/',day,' ',hour,':',minute,':',second);
+console.log(result);
+
+
+//출력
+//2019/04/26 11:34:27
+
+//concat
+//두개 이상의 배열, 배열안에 값을 추가할 때, 문자열에 문자열을 합칠 때 등 사용이 가능하다.
+//원본은 건드리지 않는다.
+
