2a7779a6aa71c4b6a05f57ab8b0c06aff6899f17
JoHyemi <92564768+JoHyemi@users.noreply.github.com>
2021-12-08 22:48:24 +0900

    문장 거꾸로 뒤집기

--- JS100/016.js
+++ JS100/016.js
@@ -7,3 +7,16 @@ reverseSentence = n.split('').reverse().join('')
 // 문자열을 일단 배열로 바꾸고(split(''))
 // 그걸 뒤집어 준다(reverse())
 // 그리고 다시 하나의 문자열로 변경시켜 준다.(join('')
+
+
+
+//function, if, slice 사용방법
+let text = prompt('문장을 입력하세요');
+function reverse(text) {
+    text += ''
+    if(text.length <=1) {
+        return text
+    }
+    return reverse(text.slice(1)) + text[0]
+}
+reverse(text);
